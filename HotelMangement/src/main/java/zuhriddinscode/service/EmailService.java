a43781efc4c911c6dc55b49ea84562bc47836876
IbrohimovZuhriddin <ibrohimovzuhriddin310@gmail.com>
2024-10-22 08:56:51 +0500

    Second attempt

--- HotelMangement/src/main/java/zuhriddinscode/service/EmailService.java
+++ HotelMangement/src/main/java/zuhriddinscode/service/EmailService.java
@@ -21,11 +21,4 @@ public class EmailService {
         mailSender.send(email);
     }
 
-//    public void sendConfirmationEmail(String to, String subject, String text) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(text);
-//        mailSender.send(message);
-//    }
 }
